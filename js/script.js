047cbf1a2df39043935fe2a45f639576ed0fdf1d
stephr8 <stephr.zh@gmail.com>
2024-05-06 11:27:33 +0200

    javascript content from pokemon

--- js/script.js
+++ js/script.js
@@ -1,29 +1,85 @@
 const app = document.querySelector('#app');
 const url = "https://api.publibike.ch/v1/public/partner/stations";
 
-// var express = require('express');
-// var cors = require('cors');
-// var app1 = express();
-
-// app1.use(cors({ origin: true, credentials:true}));
 
 
 
-
-function init(){
-
-}
-
-async function fetchData(url){
+async function fetchData(url) {
+    let url = `https://api.publibike.ch/v1/public/partner/stations`;
     try {
-        let stations = await fetch(url);
+        let stations = await fetch(url); //response in pokemon
         let data = await stations.json();
-        console.log(data.stations);
+        return data;
     }
-    catch (error){
+    catch (error) {
         console.error(error);
     }
 }
 
-fetchData(url);
+function createCard(bike) {
+    let card = document.createElement('div');
+    card.className = 'bikeCard';
+
+    let cardHeader = document.createElement('div');
+    cardHeader.className = 'cardHeader';
+
+    let nameElement = document.createElement('h2');
+    nameElement.textContent = bike.name;
+    cardHeader.appendChild(nameElement);
+    let typesContainer = document.createElement('div');
+    typesContainer.className = 'typesContainer';
+
+    bike.types.forEach(type => {
+        let typeIcon = document.createElement('div');
+        typeIcon.className = `icon ${type.type.name}`;
+        let typeSVG = document.createElement('img');
+        typeSVG.src = `./img/${type.type.name}.svg`;
+        typeSVG.alt = type.type.name;
+        typeIcon.appendChild(typeSVG);
+        typesContainer.appendChild(typeIcon);
+    });
+
+    cardHeader.appendChild(typesContainer);
+    card.appendChild(cardHeader);
+
+    //image
+
+    let detailsDiv = document.createElement('div');
+    detailsDiv.className = 'detailsDiv';
+
+    let statsList = document.createElement('ul');
+    statsList.className = 'statsList';
+
+    bike.stats.forEach(stat => {
+        let statItem = document.createElement('li');
+        let statName = document.createElement('strong');
+        statName.textContent = `${stat.stat.name}: `;
+        statItem.appendChild(statName);
+        statItem.appendChild(document.createTextNode(stat.base_stat));
+        statsList.appendChild(statItem);
+    });
+
+    detailsDiv.appendChild(statsList);
+    card.appendChild(detailsDiv);
+
+    app.appendChild(card);
+}
+
+
+
+
+
+
+// async function fetchData(url){
+//     try {
+//         let stations = await fetch(url);
+//         let data = await stations.json();
+//         console.log(data.stations);
+//     }
+//     catch (error){
+//         console.error(error);
+//     }
+// }
+
+// fetchData(url);
 
